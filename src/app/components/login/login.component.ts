6ecb31b0a8b0f9034bbcc3e42a93a9849ed9cee4
Amine1Benabbou <benabboua581@gmail.com>
2025-02-13 08:45:55 +0100

    Initial commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      pseudo: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Inscription réussie', this.loginForm.value);
+    } else {
+      console.log('Veuillez remplir tous les champs correctement');
+    }
+  }
 }
